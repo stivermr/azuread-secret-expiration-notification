f0365d436b25a697ca8accd3ff0961b9268c57aa
msti <39834706+stivermr@users.noreply.github.com>
2023-11-13 15:02:44 -0500

    Update Get-AzADAppSecretExpiration.ps1

--- Get-AzADAppSecretExpiration.ps1
+++ Get-AzADAppSecretExpiration.ps1
@@ -6,7 +6,7 @@ Import-Module Microsoft.Graph.applications
 Import-Module Microsoft.Graph.Users
 
 
-Connect-MgGraph -AccountId user1@server.com
+Connect-MgGraph
 
 
 ## UPDATE these values
@@ -16,77 +16,61 @@ $fromaddr="AzureSecretsExpiring@email.com"
 $smtpserver="smtp.email.com"
 $emailsubject = "Application Secrets Expiring Soon"
 
-Get-MgApplication -All | ForEach-Object{
-    $applicationName = $objectType = $objectId = $applicationId = $homePage = $identifierUrls = $replyUrls = $keyId = $startDate = $endDate = ""
-    $applicationName = $_.DisplayName
-    $objectId = $_.Id
-    $applicationId = $_.AppId
-    $identifierUrls = $_.IdentifierUris -Join ","
-    $secretdata = @()
-    # pull secrets
-    $secretdata = (Get-MgApplication -ApplicationId $objectId).PasswordCredentials | ForEach-Object{
-        $objectType = 'Secret'
-        $keyId = $_.KeyId
-        $startDate = $_.StartDateTime
-        $endDate = $_.EndDateTime
-        [PSCustomObject] @{
-                        'applicationName'        = $applicationName
-                        'objectType'             = $objectType
-                        'objectId'               = $objectId
-                        'applicationId'          = $applicationId
-                        'identifierUrls'         = $identifierUrls
-                        'keyId'                  = $keyId
-                        'startDate'              = $startDate
-                        'endDate'                = $endDate
-                        }
+$applications = Get-MgApplication -All
 
-    }
-    # pull certificates
-    $certdata = (Get-MgApplication -ApplicationId $objectId).KeyCredentials | ForEach-Object{
-        $objectType = $_.Type
-        $keyId = $_.KeyId
-        $startDate = $_.StartDateTime
-        $endDate = $_.EndDateTime
-        [PSCustomObject] @{
-                        'applicationName'        = $applicationName
-                        'objectType'             = $objectType
-                        'objectId'               = $objectId
-                        'applicationId'          = $applicationId
-                        'identifierUrls'         = $identifierUrls
-                        'keyId'                  = $keyId
-                        'startDate'              = $startDate
-                        'endDate'                = $endDate
-                        }
-
-    }
-    $secretdata1 = [Array]$secretdata1 + $secretdata
-    $certdata1 = [Array]$certdata1 + $certdata
-}
-# sort and filter secrets to newest
-$list1 = $secretdata1 | Group-Object -Property objectId | ForEach-Object{$_.Group | Sort-Object -Property endDate -Descending | Select-Object -First 1}
-# sort and filter certificates to newest
-$list2 = $certdata1 | Group-Object -Property objectId | ForEach-Object{$_.Group | Sort-Object -Property endDate -Descending | Select-Object -First 1}
-$masterlist = $list1 + $list2
-$masterlist = $masterlist | Sort-Object applicationName
-# limit to expiring within 30 days
+# Collect secret and certifiate information for applications expiring within 30 days
 $limit = (Get-Date).AddDays(30)
 $limit1 = Get-Date
-foreach ($l in $masterlist){
-    $data2 = if ($l.endDate -lt $limit -and $l.endDate -gt $limit1){
+$credentialsToRenew = foreach ($app in $applications) {
+    $appId = $app.AppId
+    $objectId = $app.Id
+    $appName = $app.DisplayName
+    $identifierUrls = $app.IdentifierUris -join ","
+    
+    $credentials = @()
+
+    # Retrieve and process secret credentials
+    $credentials += $app.PasswordCredentials | Where-Object { $_.EndDateTime -lt $limit -and $_.EndDateTime -gt $limit1 } | ForEach-Object {
         [PSCustomObject]@{
-            'applicationName' = $l.applicationName
-            'objectType' = $l.objectType
-            'appId' = $l.applicationId
-            'objectId' = $l.objectId
-            'keyId' = $l.keyId
-            'endDate' = $l.endDate
+            'applicationName' = $appName
+            'objectType' = 'Secret'
+            'appId' = $appId
+            'objectId' = $objectId
+            'identifierUrls' = $identifierUrls
+            'keyId' = $_.KeyId
+            'startDate' = $_.StartDateTime
+            'endDate' = $_.EndDateTime
         }
     }
-    $data3 = [Array]$data3 + $data2
+
+    # Retrieve and process certificate credentials
+    $credentials += $app.KeyCredentials | Where-Object { $_.EndDateTime -lt $limit -and $_.EndDateTime -gt $limit1 } | ForEach-Object {
+        [PSCustomObject]@{
+            'applicationName' = $appName
+            'objectType' = $_.Type
+            'appId' = $appId
+            'objectId' = $objectId
+            'identifierUrls' = $identifierUrls
+            'keyId' = $_.KeyId
+            'startDate' = $_.StartDateTime
+            'endDate' = $_.EndDateTime
+        }
+    }
+
+    $credentials
 }
-$data3 = $data3 | Sort-Object endDate
+
+# Filter out applications with names containing "SSO"
+$credentialsToRenew = $credentialsToRenew | Where-Object { $_.ApplicationName -notlike '*- AAP*' }
+
+# Output the selected credentials
+#$credentialsToRenew | Sort-Object endDate
+
+# group and sort secrets and certificates
+$applist = $credentialsToRenew | Group-Object -Property objectId | ForEach-Object{$_.Group | Sort-Object -Property endDate -Descending} |  Sort-Object applicationName
+
 # get owner of application
-$email = foreach ($d in $data3){
+$email = foreach ($d in $applist){
     $owners = @()
     $owners = Get-MgApplicationOwner -ApplicationId (Get-MgApplication -All -Filter "AppId eq '$($d.appId)'").Id
 	if ($owners){
